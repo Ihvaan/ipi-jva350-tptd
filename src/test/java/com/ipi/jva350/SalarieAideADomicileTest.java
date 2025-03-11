edd6b76e6a13a330da1450a65eb276360a6de7d9
Ihvaan <akyurek.ihvan@gmail.com>
2025-03-11 11:55:29 +0100

    Avancement

--- src/test/java/com/ipi/jva350/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/SalarieAideADomicileTest.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SalaireAideADomicileTest {
+class SalarieAideADomicileTest {
 
     @Test
     void testSalarieAideADomicile() {
